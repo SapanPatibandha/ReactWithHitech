62c83643f350ee294aa56c6bb260738fc666f000
Sapan Patibandha <patibandha@hotmail.com>
2025-10-19 01:00:12 +0530

    Props created for components

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,8 +11,9 @@ function App() {
     <>
        <h1 className="text-3xl font-bold underline">
         Hello world!
-      </h1> 
-         
+      </h1>
+      <Card userName="John Doe" />
+      <Card userName="Jane Smith" />
     </>
   )
 }
