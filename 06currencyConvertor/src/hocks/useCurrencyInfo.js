9dcf11970632c0092fadc4bc788f5ce9881e74ec
Sapan Patibandha <patibandha@hotmail.com>
2025-10-23 16:30:18 +0530

    Currency convertor is created and custom huckk is added

---
+++ 06currencyConvertor/src/hocks/useCurrencyInfo.js
@@ -0,0 +1,15 @@
+import { use, useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({});
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+        .then(res => res.json())
+        .then(data => setData(data[currency]))
+        console.log('Fetched data for', currency);  
+    },[currency])
+    console.log('useCurrencyInfo called for', currency);
+    return data;
+}
+
+export default useCurrencyInfo;
